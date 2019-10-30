9c2be6222701712e91eda8356ab1f84598f06ae3
Mike Rubits <sponge@d8d.org>
2019-10-30 08:29:52 -0500

    update server banner along with channel message

--- config.example.js
+++ config.example.js
@@ -1,5 +1,6 @@
 module.exports = {
-  images: Array(1255).fill().map((val, i) => `http://spongeserv.com/dogbot/dogs/img_${i}.jpg`),
+  basePath: 'http://spongeserv.com/dogbot/',
+  images: Array(1255).fill().map((val, i) => `dogs/img_${i}.jpg`),
   channels: [
     '',
   ],
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const Discord = require('discord.js');
+const Discord = require('./discord.js/');
 const CronJob = require('cron').CronJob;
 const Config = require('./config');
 const fs = require('fs');
+const sharp = require('sharp');
 
 async function updateDog(message) {
-  const embed = new Discord.RichEmbed();
+  const embed = new Discord.MessageEmbed();
   const image = Config.images[Math.floor(Math.random() * 1E6 ) % Config.images.length];
-  embed.setImage(image);
+  embed.setImage(Config.basePath + image);
+
+  const inset = await sharp(image).resize({width: 960, height: 540, fit: 'inside'}).toBuffer();
+
+  const newBannerBuffer = await sharp(image)
+    .resize({width: 960, height: 540, fit: 'cover'})
+    .blur(10)
+    .composite([{input: inset}])
+    .png()
+    .toBuffer();
+
+  const ret = await message.guild.setBanner(newBannerBuffer);
+
   return message.edit('Welcome to our server! Here is our code of conduct we expect all people to follow. Please check back occasionally for new and updated rules:', embed);
 }
 
@@ -15,12 +28,10 @@ async function loop() {
 
   client.on('ready', async () => {
     const id = client.user.id;
-    console.log(`Logged in as ${client.user.tag}!`);
-  
     const channels = Config.channels.map(channel => client.channels.get(channel));
   
     for (const channel of channels) {
-      const messages = await channel.fetchMessages();
+      const messages = await channel.messages.fetch();
       const myMessages = messages.filter(message => message.author.id === id);
       if (myMessages.size === 0) {
         const sentMessage = await channel.send('Waiting for server rules...');
@@ -30,7 +41,6 @@ async function loop() {
       }
     }
 
-    console.log("update done, destroying client");
     client.destroy();
   });
 
@@ -38,7 +48,7 @@ async function loop() {
 }
 
 new CronJob({
-  cronTime: '0 */4 * * *',
+  cronTime: '0 */2 * * *',
   //cronTime: '* * * * *',
   onTick: loop,
   start: true,
